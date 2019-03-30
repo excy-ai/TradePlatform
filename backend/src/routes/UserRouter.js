20aa060103aec79ae6a4c1ea7a451842ebc0a85f
Данил Проценко <deadbender27@gmail.com>
2019-03-31 01:44:08 +0600

    Delete UserRouter.js

--- backend/src/routes/UserRouter.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const Router = require('koa-router');
-const {authCheck} = require('../middlewares/AuthCheck');
-const userController = require('../controllers/UserController');
-
-module.exports = new Router()
-    .get('/me', authCheck, userController.getMe);
