0706a4d32a0b787067bea5833e41b735c55a7635
exc <DeadBender27@gmail.com>
2019-04-03 10:28:33 +0600

    [back] some pagination fixes

--- backend/src/controllers/marketController.js
+++ backend/src/controllers/marketController.js
@@ -4,7 +4,6 @@ const {Item, User} = require('../models');
 const PAGINATION_LIMIT = 25;
 
 async function getAllOnTrade(ctx) {
-    let offset = 0;
     let {category, order, page, userID} = ctx.request.body;
 
     if (!order || (order !== "ASC" && order !== "DESC")) {
@@ -18,97 +17,80 @@ async function getAllOnTrade(ctx) {
         }
     }
     let items;
-    let pages;
     let count;
     if (category && inventoryId) {
         count = (await Item.findAndCountAll({
             where: {onTrade: true, category: category, inventory_id: inventoryId},
 
         })).count;
-        pages = Math.ceil(count / PAGINATION_LIMIT);
-        if (!page || page<=0) {
-            page = 1;
-        } else if (page > pages) {
-            page = pages;
-        }
-        offset = PAGINATION_LIMIT * (page - 1);
-
         items = await Item.findAll({
             where: {onTrade: true, category: category, inventory_id: inventoryId},
             order: [
                 ['created_at', order]
             ],
             limit: PAGINATION_LIMIT,
-            offset: offset,
+            offset: getOffset(count, page),
         });
     } else if (category) {
         count = (await Item.findAndCountAll({
             where: {onTrade: true, category: category},
 
         })).count;
-        pages = Math.ceil(count / PAGINATION_LIMIT);
-        if (!page || page<=0) {
-            page = 1;
-        } else if (page > pages) {
-            page = pages;
-        }
-        offset = PAGINATION_LIMIT * (page - 1);
-
         items = await Item.findAll({
             where: {onTrade: true, category: category},
             order: [
                 ['created_at', order]
             ],
             limit: PAGINATION_LIMIT,
-            offset: offset,
+            offset: getOffset(count, page),
         });
     } else if (userID) {
         count = (await Item.findAndCountAll({
             where: {onTrade: true, inventory_id: inventoryId},
 
         })).count;
-        pages = Math.ceil(count / PAGINATION_LIMIT);
-        if (!page || page<=0) {
-            page = 1;
-        } else if (page > pages) {
-            page = pages;
-        }
-        offset = PAGINATION_LIMIT * (page - 1);
-
         items = await Item.findAll({
             where: {onTrade: true, inventory_id: inventoryId},
             order: [
                 ['created_at', order]
             ],
             limit: PAGINATION_LIMIT,
-            offset: offset,
+            offset: getOffset(count, page),
         });
     } else {
         count = (await Item.findAndCountAll({
             where: {onTrade: true},
 
         })).count;
-        pages = Math.ceil(count / PAGINATION_LIMIT);
-        if (!page || page<=0) {
-            page = 1;
-        } else if (page > pages) {
-            page = pages;
-        }
-        offset = PAGINATION_LIMIT * (page - 1);
-
         items = await Item.findAll({
             where: {onTrade: true},
             order: [
                 ['created_at', order]
             ],
             limit: PAGINATION_LIMIT,
-            offset: offset,
+            offset: getOffset(count, page),
         });
     }
-    ctx.response.body = items;
+    let pages = Math.ceil(count / PAGINATION_LIMIT);
+    ctx.response.body = {
+        pages: pages,
+        items
+    };
     ctx.response.status = 200;
     return ctx.response;
 }
 
+function getOffset(count, page) {
+    let pages = Math.ceil(count / PAGINATION_LIMIT);
+    if (!page || page <= 0) {
+        page = 1;
+    } else if (page > pages && pages !== 0) {
+        page = pages;
+    } else if (pages === 0) {
+        page = 1;
+    }
+    let offset = PAGINATION_LIMIT * (page - 1);
+    return offset;
+}
 
 module.exports = {getAllOnTrade};
