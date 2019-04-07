7c3faf2039a1b904e8e16916d0dacf686b6be1d2
exc <DeadBender27@gmail.com>
2019-04-07 13:45:09 +0600

    added simple test (template)

--- backend/src/controllers/categoryLoader.js
+++ backend/src/controllers/categoryLoader.js
@@ -1,19 +1,11 @@
 'use strict';
 
-// const fs = require('fs');
-// const categoryList = fs.readFileSync('../cfg/CategoryList.json', 'utf8');
+const path = require('path');
+const fs = require('fs');
+const categoryList = fs.readFileSync(path.resolve(__dirname,'../cfg/CategoryList.json'), 'utf8');
 const {Category} = require('../models');
 
-// let categorys = JSON.parse(categoryList).data;
-
-let categorys = [
-    "common",
-    "uncommon",
-    "rare",
-    "mythical",
-    "legendary",
-    "immortal"
-];
+let categorys = JSON.parse(categoryList).data;
 
 async function insertCategorys() {
     await categorys.map((category) => {
