730805cb0910b765b2f758c0d07452c80775c8a9
exc- <deadbender27@gmail.com>
2019-05-03 14:42:22 +0600

    offerconfirm added

--- frontend/src/pages/offers/OfferConfirm.js
+++ frontend/src/pages/offers/OfferConfirm.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
-export default function OfferConfirm(props) {
-
+function OfferConfirm(props) {
+  useEffect(() => {
+    // if (!props.creating) {
+    //   props.history.push('/me');
+    // } TODO: fix bug when creating blinks to false
+  });
+  console.log(props.targetItem);
+  console.log(props.selectedItem);
+  //TODO: create confirm button that creates offer
   return (<React.Fragment>
     Data is loading
   </React.Fragment>);
 }
+
+const mapStateToProps = state => ({
+  targetItem: state.offerReducer.targetItem,
+  selectedItem: state.offerReducer.selectedItem,
+});
+
+const mapDispatchToProps = dispatch => ({});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(OfferConfirm);
